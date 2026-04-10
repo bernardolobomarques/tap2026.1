8d2ddb72cca6d676c301af0dd10ae0ca623c91e1
BERNARDO LOBO MARQUES <202401709433@alunos.ibmec.edu.br>
2026-04-10 14:55:17 -0300

    Apagando arquivos antigos

--- pedidos/src/cleancode/model/StatusPedido.java
+++
@@ -1,8 +0,0 @@
-package cleancode.model;
-
-// Classe removida - status agora eh uma String simples ("novo", "cancelado")
-// Mantida para nao quebrar historico do git
-@Deprecated
-public class StatusPedido {
-    // nao usar mais - veja Pedido.getStatus()
-}
--- pedidos/src/cleancode/model/TipoCliente.java
+++
@@ -1,8 +0,0 @@
-package cleancode.model;
-
-// Classe removida - tipo do cliente agora eh uma String simples ("comum", "premium", "vip")
-// Mantida para nao quebrar historico do git
-@Deprecated
-public class TipoCliente {
-    // nao usar mais - veja Cliente.getTipo()
-}
